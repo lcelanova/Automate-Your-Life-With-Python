2644087c46c6de17a28f7401e8c77ba36a1f4f30
lcelanova <114341200+lcelanova@users.noreply.github.com>
2023-01-24 13:40:11 +0100

    crypto-news1

---
+++ crypto-news.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+website = "https://www.coindesk.com/livewire/"
+path = '/usr/local/bin/chromedriver'
+s=Service(executable_path=path)
+driver = webdriver.Chrome(service=s)
+
+driver.get(website)
+
+containers = driver.find_elements(by="xpath", value='//div[@class="article-cardstyles__AcTitle-q1x8lc-1 bzAuaw articleTextSection "]')
+
+titles = []
+links = []
+
+for container in containers:
+	title = container.find_element(by="xpath", value='./h5').text
+	link = container.find_element(by="xpath", value='./h5/a').get_attribute("href")
+
+	titles.append(title)
+	links.append(link)
+
+driver.quit()
+
+mydictionary = {'Title': titles, 'Link': links}
+
+df_news = pd.DataFrame(mydictionary)
+df_news.to_csv('cryptonews.csv')
+
